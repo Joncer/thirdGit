1d8d099c197d5cbc430cd4190a443a5d46a99eca
Joncer <229823811@qq.com>
2014-01-14 15:37:17 +0800

    在LoginAcitity.java中添加了一个空格，测试分支

--- src/com/cs/test5/LoginActivity.java
+++ src/com/cs/test5/LoginActivity.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.View.OnClickListener; 
 import android.widget.Button;
 import android.widget.TextView;
 
